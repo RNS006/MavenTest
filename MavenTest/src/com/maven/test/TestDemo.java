8a40d10e917bb6e6680b22ea9fe44e895c4a0d34
rudra006-admin <rudrag.scm@gmail.com>
2021-04-20 21:10:21 +0530

    added TestDemo.java

---
+++ MavenTest/src/com/maven/test/TestDemo.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.maven.test;
+
+/**
+ * @author Rudra
+ *
+ */
+public class TestDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome To maven Test Project");
+
+	}
+
+}
